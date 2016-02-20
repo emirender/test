a7ab76277006098cd8a667fb67b5379c3d11b6f8
IOS 3401 <ios3401@lab-g-imac-20.local>
2016-02-20 11:48:28 +0200

    terminator komutu

--- Test/main.swift
+++ Test/main.swift
@@ -11,4 +11,28 @@ import Foundation
 print("Merhaba")
 
 var myVar = 30.0
+let myConst = 7
 print(myVar)
+
+var myFloat:Float = 3.14
+
+var result:Float = myFloat + Float(myConst)
+print(result)
+
+let apples = 30
+let oranges = 20
+let appleSummary = "\(apples) tane elma var."
+let fruitSummary = "\(apples + oranges) tane elma var."
+print(appleSummary)
+print (fruitSummary)
+
+
+for i in 0..<10 {
+    print(i)
+}
+
+var birliste = ["sıra", "masa", "kapı", "dolap", "çanta"]
+for eleman in birliste {
+    print(eleman, terminator:"")
+}
+print("")
