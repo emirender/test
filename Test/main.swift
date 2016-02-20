edb96a9dc1f9fb5e26f9bd613076866a1b68942e
IOS 3401 <ios3401@lab-g-imac-20.local>
2016-02-20 12:22:32 +0200

    ders devam

--- Test/main.swift
+++ Test/main.swift
@@ -33,6 +33,56 @@ for i in 0..<10 {
 
 var birliste = ["sıra", "masa", "kapı", "dolap", "çanta"]
 for eleman in birliste {
-    print(eleman, terminator:"")
+    print(eleman)
 }
 print("")
+
+print(birliste[4])
+
+var meslekler = [
+"Serkan":"Mühendis",
+"Gözde":"Ajan",
+]
+
+print(meslekler)
+
+meslekler ["Gözde"] = "Yazar"
+print(meslekler)
+print (meslekler ["Serkan"])
+print ("---")
+
+for meslek in meslekler {
+    print (meslek.1)
+}
+
+var notlar = [
+"Serkan": [10, 90, 50],
+"Gözde": [100, 0 , 30]
+]
+
+print ("---")
+for ogrenci in notlar {
+    print (ogrenci.1[1])
+}
+
+print(notlar["Serkan"]?[0])
+print("---")
+
+let skorlar = [23,45,10,7,6,55,10]
+var takımSkoru = 0
+for skor in skorlar {
+    if skor > 40 {
+            takımSkoru += 3
+    } else {
+            takımSkoru += 1
+    }
+}
+
+print (takımSkoru)
+
+var optionalString:String? = "Selam"
+print(optionalString == nil)
+if let name = optionalString {
+    print("nsc")
+
+}
